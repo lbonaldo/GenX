e372f9525068bfe4a9ec1e48e2f860b3f1c78800
lbonaldo <bonaldo.luca12@gmail.com>
2023-10-10 11:50:19 -0400

    Add a new test case for the PiecewiseFuel CO2 module

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,10 +17,19 @@ end
     include("expression_manipulation_test.jl")
 end
 
+# Test GenX modules
 @testset "Time domain reduction" begin
     include("time_domain_reduction.jl")
 end
 
+@testset "PiecewiseFuel CO2" begin
+    include("PiecewiseFuel_CO2.jl")
+end 
+
 @testset "VRE and storage" begin
     include("VREStor.jl")
+end
+
+@testset "Electrolyzer" begin
+    include("electrolyzer.jl")
 end
--- test/utilities.jl
+++ test/utilities.jl
@@ -5,21 +5,21 @@ using Logging, LoggingExtras
 
 
 function solve_genx_model_testing(genx_setup::Dict, test_path::AbstractString)
-    # Suppress printing on console
-    console_out = stdout
-    redirect_stdout(devnull)
+    # Create a ConsoleLogger that prints any log messages with level >= Warn to stderr
+    warnerror_logger = ConsoleLogger(stderr, Logging.Warn)
 
-    # Run the case
-    OPTIMIZER = configure_solver(genx_setup["Solver"], test_path)
-    inputs = load_inputs(genx_setup, test_path)
-    EP = generate_model(genx_setup, inputs, OPTIMIZER)
-    EP, _ = solve_model(EP, genx_setup)
+    # Redirect all log messages with level >= Warn to stderr
+    EP, _ = with_logger(warnerror_logger) do
+        # Run the case
+        OPTIMIZER = configure_solver(genx_setup["Solver"], test_path)
+        inputs = load_inputs(genx_setup, test_path)
+        EP = generate_model(genx_setup, inputs, OPTIMIZER)
+        solve_model(EP, genx_setup)
+    end
+
     # Get and return the objective value and tolerance
     obj_test = objective_value(EP)
     optimal_tol = get_attribute(EP, "dual_feasibility_tolerance")
-
-    # Restore printing on console
-    redirect_stdout(console_out)
 
     return obj_test, optimal_tol
 end
@@ -33,7 +33,12 @@ function write_testlog(test_path::AbstractString, obj_test::Real, optimal_tol::R
     end
 
     log_file_path = joinpath("Logs", "$(test_path).log")
-    logger = FileLogger(log_file_path; append = true)
+
+    logger = FormatLogger(open(log_file_path, "a")) do io, args
+        # Write only the message
+        println(io, args.message)
+    end
+    
     with_logger(logger) do
         time = Dates.format(now(), "yyyy-mm-dd HH:MM:SS")
         @info "$time\t$obj_test ± $optimal_tol\t$test_result"
